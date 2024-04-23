2caeacf9e39d97dedfcf6b36158398cfd273e5b6
Andrew Platonov <andrew.platonov@protonmail.com>
2024-04-23 10:36:40 +0200

    Linter fixes.

--- jerr/const.go
+++ jerr/const.go
@@ -21,9 +21,9 @@ const (
 	InfoIsEmpty        = "the INFO directive cannot be empty"
 	ResponsesIsEmpty   = "the response cannot be empty"
 	RequestIsEmpty     = "the request cannot be empty"
-	DescriptionIsEmpty = "the description cannot be empty, learn more about the Description directive here: https://jsight.io/docs/jsight-api-0-3#directive-description"
+	DescriptionIsEmpty = "the description cannot be empty, learn more about the Description directive here: https://jsight.io/docs/jsight-api-0-3#directive-description" //nolint:lll
 	BodyIsEmpty        = "the body cannot be empty"
-	MacroIsEmpty       = "the macros cannot be empty, learn more about the MACRO directive here: https://jsight.io/docs/jsight-api-0-3#directive-macro"
+	MacroIsEmpty       = "the macros cannot be empty, learn more about the MACRO directive here: https://jsight.io/docs/jsight-api-0-3#directive-macro" //nolint:lll
 
 	IncorrectPath             = "incorrect path"
 	IncorrectRequest          = "incorrect request"
@@ -31,15 +31,15 @@ const (
 	IncorrectParameter        = "incorrect parameter"
 
 	PathOrErr                          = "the root schema object cannot have the `or` rule in the Path directive"
-	PathObjectErr                      = "the body of the Path directive must be an object, learn more about the Path directive here: https://jsight.io/docs/jsight-api-0-3#directive-path"
-	PathAdditionalPropertiesErr        = `the "additionalProperties" rule should not be used in the Path directive, learn more about the Path directive here: https://jsight.io/docs/jsight-api-0-3#directive-path`
-	PathNullableErr                    = `the "nullable" rule should not be used in the Path directive, learn more about the Path directive here: https://jsight.io/docs/jsight-api-0-3#directive-path`
-	PathEmptyErr                       = "the object in the Path directive can not be empty, learn more about the Path directive here: https://jsight.io/docs/jsight-api-0-3#directive-path"
+	PathObjectErr                      = "the body of the Path directive must be an object, learn more about the Path directive here: https://jsight.io/docs/jsight-api-0-3#directive-path"                         //nolint:lll
+	PathAdditionalPropertiesErr        = `the "additionalProperties" rule should not be used in the Path directive, learn more about the Path directive here: https://jsight.io/docs/jsight-api-0-3#directive-path` //nolint:lll
+	PathNullableErr                    = `the "nullable" rule should not be used in the Path directive, learn more about the Path directive here: https://jsight.io/docs/jsight-api-0-3#directive-path`             //nolint:lll
+	PathEmptyErr                       = "the object in the Path directive can not be empty, learn more about the Path directive here: https://jsight.io/docs/jsight-api-0-3#directive-path"                        //nolint:lll
 	PathMultiLevelPropertyErr          = "the multi-level property is not allowed in the Path directive"
 	PathEmptyParameter                 = "empty PATH parameter"
 	PathParameterIsDuplicatedInThePath = "the parameter of the path is duplicated"
-	PathsAreSimilar                    = "the ambiguous paths are not allowed: \"/%s/{%s}\", \"/%s\", see the details here: https://jsight.io/docs/jsight-api-0-3#parameter-path"
-	PathParameterAlreadyDefined        = "The parameter %q has already been defined earlier, see more details about path parameters here: https://jsight.io/docs/jsight-api-0-3#parameter-path"
+	PathsAreSimilar                    = "the ambiguous paths are not allowed: \"/%s/{%s}\", \"/%s\", see the details here: https://jsight.io/docs/jsight-api-0-3#parameter-path"               //nolint:lll
+	PathParameterAlreadyDefined        = "The parameter %q has already been defined earlier, see more details about path parameters here: https://jsight.io/docs/jsight-api-0-3#parameter-path" //nolint:lll
 
 	IncludeRootErr      = "cannot not start with `/`"
 	IncludeUpErr        = "cannot contain `..` or `.`"
@@ -62,18 +62,18 @@ const (
 	AnnotationIsForbiddenForTheDirective                = "the annotation is not allowed for this directive"
 	NotUniqueDirective                                  = "the directive has already been defined"
 	NotUniquePath                                       = "the path %q has already been defined"
-	BodyMustBeObject                                    = "there must be an object or a reference to an object in the directive body"
-	CannotUseTheTypeAndSchemaNotationParametersTogether = "directive parameters `Type` and `SchemaNotation` cannot be declared simultaneously"
-	ThereIsNoExplicitContextForClosure                  = "nothing to close with this closing parenthesis, learn more about the explicit direcitve boundaries here: https://jsight.io/docs/jsight-api-0-3#boundaries-of-the-body-of-the-directive"
+	BodyMustBeObject                                    = "there must be an object or a reference to an object in the directive body"                                                                                                              //nolint:lll
+	CannotUseTheTypeAndSchemaNotationParametersTogether = "directive parameters `Type` and `SchemaNotation` cannot be declared simultaneously"                                                                                                     //nolint:lll
+	ThereIsNoExplicitContextForClosure                  = "nothing to close with this closing parenthesis, learn more about the explicit direcitve boundaries here: https://jsight.io/docs/jsight-api-0-3#boundaries-of-the-body-of-the-directive" //nolint:lll
 	DirectiveNotAllowed                                 = "the directive is not allowed"
 	ApartFromTheOpeningParenthesis                      = "apart from the opening parenthesis, there should be nothing else on this line, learn more about the explicit direcitve boundaries here: https://jsight.io/docs/jsight-api-0-3#boundaries-of-the-body-of-the-directive" //nolint:lll
 	DuplicateNames                                      = "the name %q has already been declared before"
-	NotAllowedToOverrideTheProperty                     = "it is not allowed to override the %q property from the user type %q" //nolint:lll
-	ContextNotClosed                                    = "this opening paranthesis is not closed, learn more about the explicit direcitve boundaries here: https://jsight.io/docs/jsight-api-0-3#boundaries-of-the-body-of-the-directive"
+	NotAllowedToOverrideTheProperty                     = "it is not allowed to override the %q property from the user type %q"                                                                                                            //nolint:lll
+	ContextNotClosed                                    = "this opening parenthesis is not closed, learn more about the explicit direcitve boundaries here: https://jsight.io/docs/jsight-api-0-3#boundaries-of-the-body-of-the-directive" //nolint:lll
 	WrongDescriptionContext                             = "wrong description context"
 	MethodIsAlreadyDefinedInResource                    = "this method has already been defined in the resource"
 	UndefinedRequestBodyForResource                     = "undefined request body for resource"
-	RecursionIsProhibited                               = "file dependency recursion is detected, learn more about the INCLUDE directive here: https://jsight.io/docs/jsight-api-0-3#directive-include"
+	RecursionIsProhibited                               = "file dependency recursion is detected, learn more about the INCLUDE directive here: https://jsight.io/docs/jsight-api-0-3#directive-include" //nolint:lll
 	UserTypeIsNotAnObject                               = "the user type is not an object"
 	ProcessTypeErr                                      = "process type"
 	FailedToComputeScannersHash                         = "failed to compute the scanner's hash"
